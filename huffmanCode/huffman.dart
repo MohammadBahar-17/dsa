6c0fb9e546bcd03305c47bb2253ccd71494099d5
Mohammad Bahar <211053@ppu.edu.ps>
2025-03-28 18:40:22 +0300

    change huffman

--- huffmanCode/huffman.dart
+++ huffmanCode/huffman.dart
@@ -1,90 +1,86 @@
-class HuffmanNode {
-  String? character;
-  int frequency;
-  HuffmanNode? left;
-  HuffmanNode? right;
-  HuffmanNode(this.character, this.frequency, {this.left, this.right});
+class Node {
+  String? symbol;
+  double frequency;
+  Node? left, right;
+
+  Node(this.symbol, this.frequency, {this.left, this.right});
+
+  bool get isLeaf => left == null && right == null;
 }
 
-class HuffmanCoding {
-  late Map<String, String> huffmanCode;
-  HuffmanNode? root;
+class HuffmanEncoder {
+  Map<String, String> encodingTable = {};
+  Node? root;
 
-  void buildHuffmanTree(Map<String, int> frequencyMap) {
-    List<HuffmanNode> nodes = frequencyMap.entries
-        .map((entry) => HuffmanNode(entry.key, entry.value))
+  HuffmanEncoder(Map<String, double> frequencyMap) {
+    if (frequencyMap.isEmpty) {
+      throw ArgumentError("Frequency map cannot be empty.");
+    }
+
+    List<Node> nodes = frequencyMap.entries
+        .map((entry) => Node(entry.key, entry.value))
         .toList();
 
+    nodes.sort((a, b) => a.frequency.compareTo(b.frequency));
+
     while (nodes.length > 1) {
+      var first = nodes.removeAt(0);
+      var second = nodes.removeAt(0);
+      var parent = Node(null, first.frequency + second.frequency,
+          left: first, right: second);
+      nodes.add(parent);
       nodes.sort((a, b) => a.frequency.compareTo(b.frequency));
-
-      HuffmanNode left = nodes.removeAt(0);
-      HuffmanNode right = nodes.removeAt(0);
-
-      int combinedFrequency = left.frequency + right.frequency;
-      HuffmanNode parentNode =
-          HuffmanNode(null, combinedFrequency, left: left, right: right);
-      nodes.add(parentNode);
     }
 
     root = nodes.first;
-    huffmanCode = _generateCodes(root, "");
+    _buildEncodingTable(root, "");
   }
 
-  Map<String, String> _generateCodes(HuffmanNode? node, String code) {
-    Map<String, String> codes = {};
-    if (node == null) return codes;
-
-    if (node.character != null) {
-      codes[node.character!] = code;
-    } else {
-      codes.addAll(_generateCodes(node.left, code + "0"));
-      codes.addAll(_generateCodes(node.right, code + "1"));
+  void _buildEncodingTable(Node? node, String code) {
+    if (node == null) return;
+    if (node.isLeaf && node.symbol != null) {
+      encodingTable[node.symbol!] = code.isNotEmpty ? code : "0";
+      return;
     }
-
-    return codes;
+    _buildEncodingTable(node.left, code + "0");
+    _buildEncodingTable(node.right, code + "1");
   }
 
   String encode(String text) {
-    return text.split('').map((char) => huffmanCode[char] ?? "").join();
+    return text.split('').map((char) => encodingTable[char] ?? "").join();
   }
 
-  String decode(String encodedText) {
-    StringBuffer decodedText = StringBuffer();
-    HuffmanNode? currentNode = root;
+  String decode(String binary) {
+    var result = StringBuffer();
+    Node? currentNode = root;
 
-    for (var bit in encodedText.split('')) {
+    for (var bit in binary.split('')) {
       currentNode = (bit == '0') ? currentNode?.left : currentNode?.right;
-
-      if (currentNode?.character != null) {
-        decodedText.write(currentNode!.character);
+      if (currentNode != null && currentNode.isLeaf) {
+        result.write(currentNode.symbol);
         currentNode = root;
       }
     }
-
-    return decodedText.toString();
+    return result.toString();
   }
 }
 
 void main() {
-  Map<String, int> frequencyMap = {
-    'a': 5,
-    'b': 9,
-    'c': 12,
-    'd': 13,
-    'e': 16,
-    'f': 45,
-  };
+  Map<String, double> frequencies = {'X': 0.1, 'Y': 0.15, 'Z': 0.3, 'W': 0.45};
 
-  HuffmanCoding huffman = HuffmanCoding();
-  huffman.buildHuffmanTree(frequencyMap);
+  var huffman = HuffmanEncoder(frequencies);
 
-  print("Huffman Codes: ${huffman.huffmanCode}");
+  print("Generated Huffman Codes:");
+  huffman.encodingTable.forEach((symbol, code) {
+    print("'$symbol': $code");
+  });
 
-  String text = "abcdef";
-  String encodedText = huffman.encode(text);
-  print("Encoded Text: $encodedText");
+  // Encoding
+  String inputText = "XYZ";
+  String encodedText = huffman.encode(inputText);
+  print("Encoded '$inputText': $encodedText");
 
-  String decodedText = huffman.decode(encodedText);
-  print("Decoded Text: $decodedText");
+  // Decoding
+  String encodedBinary = "11001";
+  print("Decoded '$encodedBinary': ${huffman.decode(encodedBinary)}");
 }
